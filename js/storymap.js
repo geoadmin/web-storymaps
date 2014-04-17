8533db17d2942a44899f1f8ca36be476df14a26b
Marc Monnerat <procrastinatio@gmail.com>
2014-04-17 14:38:44 +0200

    youtube fluid width

--- js/storymap.js
+++ js/storymap.js
@@ -108,3 +108,52 @@ map.on('singleclick', function(evt) {
   displayFeatureInfo(pixel, coordinate);
 });
 } 
+
+/* 
+ * Fluid width youtoub videos
+ * 
+ * By Chris Coyier & tweaked by Mathias Bynens
+ * See http://css-tricks.com/fluid-width-youtube-videos/
+ */
+
+$(function() {
+
+    // Find all YouTube videos
+    var $allVideos = $("iframe[src^='http://www.youtube.com']"),
+
+        // The element that is fluid width
+        $fluidEl = $("#feature-info");
+
+    // Figure out and save aspect ratio for each video
+    $allVideos.each(function() {
+        console.log(this);
+
+        $(this)
+            .data('aspectRatio', this.height / this.width)
+            
+            // and remove the hard coded width/height
+            .removeAttr('height')
+            .removeAttr('width');
+
+    });
+
+    // When the window is resized
+    // (You'll probably want to debounce this)
+    $(window).resize(function() {
+
+        var newWidth = $fluidEl.width();
+        
+        // Resize all videos according to their own aspect ratio
+        $allVideos.each(function() {
+
+            var $el = $(this);
+            $el
+                .width(newWidth)
+                .height(newWidth * $el.data('aspectRatio'));
+
+        });
+
+    // Kick off one resize to fix all videos on page load
+    }).resize();
+
+});
