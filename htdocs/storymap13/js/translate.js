0859b9e79a6d1891977e38082ee52f362a10c22f
Marc Monnerat <procrastinatio@gmail.com>
2014-06-30 21:35:01 +0200

    initial commit

---
+++ htdocs/storymap13/js/translate.js
@@ -0,0 +1,27 @@
+// translate text elements according to lang variable
+i18n.init({
+  lng: lang
+}, function(t) {
+  $('.multilang').i18n();
+});
+
+
+console.lang(lang);
+
+
+$(document).ready(function() {
+  if (lang == 'de') {
+    $('title').html('Die wärmsten Fliessgewässer der Schweiz');
+  }
+  if (lang == 'fr') {
+    $('title').html('Les cours d’eau les “plus chauds“ de Suisse');
+  }
+  if (lang == 'it') {
+    $('title').html('I fiumi “più caldi” della Svizzera');
+  }
+  if (lang == 'en') {
+    $('title').html('The warmest rivers of Switzerland');
+  }
+
+
+});
